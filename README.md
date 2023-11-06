6d9b9c4a00201c87480781a5f077e50e9adc2f36
ymtm123 <yamazaki@kanagawa-u.ac.jp>
2023-11-06 12:29:23 +0900

    main.pyの追加

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 - それぞれにおいて特定の路線でも分析可能
 
 ## 細かい設定
-- 全ての関数
+- 全ての関数において
   - graph_widthでグラフの横幅を設定可能
   - graph_heightでグラフの縦幅を設定可能
     - 例：df.ranking_bar("間取り", "面積", graph_width=8, graph_height=6)
-- scatter_line()とscatter_station()
+- scatter_line()とscatter_station()において
   - xlimで横軸の目盛の範囲を設定可能
   - ylimで横軸の目盛の範囲を設定可能
     - 例：df.scatter_line("面積", "家賃", xlim=(20, 40), ylim=(6, 10))
---
+++ main.py
@@ -0,0 +1,25 @@
+from utils import suumo
+import importlib
+importlib.reload(suumo)
+
+text = """
+名称、カテゴリー、アドレス、アクセス
+路線、駅、バス停、
+MMまで、バス、徒歩、車、合計時間
+築年数、構造、階数、
+家賃、管理費、敷金、礼金
+間取り、面積、URL
+"""
+
+# データの読み込み
+df = suumo.read_csv("./data/yokohama_kawasaki.csv", index_col=0, encoding="utf-8")
+
+# データ分析
+df.n_rooms("カテゴリー")
+# df.n_rooms_by_line(["京急本線", "京急逗子線"], "カテゴリー")
+
+# df.ranking_bar("間取り", "面積")
+# df.ranking_bar_by_line(["京急本線", "京急逗子線"], "間取り", "面積")
+
+# df.scatter_line("面積", "合計時間")
+# df.scatter_station(["京急本線", "京急逗子線"], "面積", "合計時間")
