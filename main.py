9748e0a161f991bff06cc0ae6a14fdca2c86fa2c
ymtm123 <yamazaki@kanagawa-u.ac.jp>
2023-11-07 09:28:00 +0900

    コメントアウトの変更

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ MMまで、バス、徒歩、車、合計時間
 df = suumo.read_csv("./data/yokohama_kawasaki.csv", index_col=0, encoding="utf-8")
 
 # データ分析
-# df.n_rooms("カテゴリー", graph_width=6, graph_height=6)
+df.n_rooms("カテゴリー")
 # df.n_rooms_by_line(["京急本線", "京急逗子線"], "カテゴリー")
 
-df.average_bar("間取り", "面積")
-df.average_bar_by_line(["京急本線", "京急逗子線"], "間取り", "面積")
+# df.average_bar("間取り", "面積")
+# df.average_bar_by_line(["京急本線", "京急逗子線"], "間取り", "面積")
 
 # df.scatter_line("面積", "合計時間")
 # df.scatter_station(["京急本線", "京急逗子線"], "面積", "合計時間")
